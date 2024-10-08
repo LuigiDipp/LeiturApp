7b4c1db72246cfe17f0a957db20e902758a0df99
machadoo <108826488+LuigiDipp@users.noreply.github.com>
2024-10-08 10:14:28 -0300

    Commit

--- app/src/main/java/com/example/leiturapp/MainActivity2.java
+++ app/src/main/java/com/example/leiturapp/MainActivity2.java
@@ -1,6 +1,8 @@
 package com.example.leiturapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,13 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        findViewById(R.id.imgMenino).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent tela = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(tela);
+            }
         });
     }
 }
