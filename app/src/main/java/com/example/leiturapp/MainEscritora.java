0367fad75c823d4d1e6709c86a21b52b9f54aebe
João Machado <joaopedropiguinho@gmail.com>
2024-10-10 11:40:03 -0300

    tela Download

--- app/src/main/java/com/example/leiturapp/MainEscritora.java
+++ app/src/main/java/com/example/leiturapp/MainEscritora.java
@@ -18,7 +18,7 @@ public class MainEscritora extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_escritora);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.MainDownload), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
