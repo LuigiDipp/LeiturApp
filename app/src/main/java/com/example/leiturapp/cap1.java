72b3270ea4d02d0e9b055a6fa28bfa48b669a430
João Machado <joaopedropiguinho@gmail.com>
2024-10-21 08:10:17 -0300

    cap

--- app/src/main/java/com/example/leiturapp/cap1.java
+++ app/src/main/java/com/example/leiturapp/cap1.java
@@ -20,5 +20,7 @@ public class cap1 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
+
 }
